ca96e783b16c90cc83e288c38cb9c2ad091c544d
lakpahana <agslakpahana@gmail.com>
2024-04-03 02:36:06 +0530

    axios with timeout

--- api/proxy.js
+++ api/proxy.js
@@ -1,38 +1,42 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 
 export default async (req, res) => {
-  
+  const baseUrl = 'https://kattis-badge-go-empty-brook-9555.fly.dev';
+  const additionalPath = req.url.replace(/^\/api\/proxy/, '');
+  const targetUrl = `${baseUrl}${additionalPath}`;
 
- const baseUrl = 'https://kattis-badge-go-empty-brook-9555.fly.dev';
+  console.log(targetUrl);
 
- const additionalPath = req.url.replace(/^\/api\/proxy/, '');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
- const targetUrl = `${baseUrl}${additionalPath}`;
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
-  console.log(targetUrl)
+  try {
+    const axiosConfig = {
+      method: req.method,
+      headers: req.headers,
+      timeout: 5000,
+    };
 
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'POST') {
+      axiosConfig.data = req.body;
+    }
 
-if (req.method === 'OPTIONS') {
-  return res.status(200).end();
-}
- try {
-   const response = await fetch(targetUrl, {
-     method: req.method,
-     headers: req.headers,
-     body: req.method === 'POST' ? JSON.stringify(req.body) : undefined,
-   });
-   
-   const data = await response.text();
-   res.status(response.status);
-   for (const [key, value] of response.headers) {
-     res.setHeader(key, value);
-   }
-   res.send(data);
- } catch (error) {
-   console.error('Error proxying the request:', error);
-   res.status(500).send(`Internal Server Error: ${error.message}`);
- }
+    const response = await axios(targetUrl, axiosConfig);
+
+    res.status(response.status);
+
+    for (const [key, value] of Object.entries(response.headers)) {
+      res.setHeader(key, value);
+    }
+
+    res.send(response.data);
+  } catch (error) {
+    console.error('Error proxying the request:', error);
+    res.status(500).send(`Internal Server Error: ${error.message}`);
+  }
 };
